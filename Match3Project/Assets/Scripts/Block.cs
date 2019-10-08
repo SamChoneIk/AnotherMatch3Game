c8d23cd2b25de34275055c1c18c547076bd24340
tizmaos01 <tizmaos01@gmail.com>
2019-10-08 19:07:26 +0900

    블럭 생성
    블럭 생성

---
+++ Match3Project/Assets/Scripts/Block.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Block : MonoBehaviour
+{
+    public int column;
+    public int row;
+    private GameHandler board;
+    private GameObject otherDot;
+    private Vector2 firstTouchPosition;
+    private Vector2 finalTouchPosition;
+    public float swipeAngle = 0;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+    private void OnMouseDown()
+    {
+        firstTouchPosition = Camera.main.ScreenToWorldPoint( Input.mousePosition);
+        Debug.Log(firstTouchPosition);
+
+    }
+
+    private void OnMouseUp()
+    {
+        finalTouchPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        CalculateAngle();
+    }
+
+    private void CalculateAngle()
+    {
+        swipeAngle = Mathf.Atan2(finalTouchPosition.y - firstTouchPosition.y, finalTouchPosition.x - firstTouchPosition.x) * 180 / Mathf.PI;
+        Debug.Log(swipeAngle);
+    }
+
+    private void MovePieces()
+    {   // Right Swipe
+        if(swipeAngle > -45 && swipeAngle <= 45)
+        {
+          
+        }
+    }
+}
---
+++ Match3Project/Assets/Scripts/GameHandler.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameHandler : MonoBehaviour
+{
+    public int width;
+    public int height;
+    public GameObject piecePrefab;
+    public GameObject[] pieces;
+    public GameObject[,] allDots;
+
+    private BlockPiece[,] board;
+    
+
+    void Start()
+    {
+        board = new BlockPiece[width, height];
+        allDots = new GameObject[width, height];
+        Init();
+    }
+
+    private void Init()
+    {
+        for (int column = 0; column < height; column++)
+        {
+            for (int row = 0; row < width; row++)
+            {
+                Vector2 tempPosition = new Vector2(row, column); // 넓이, 높이만큼 보드를 생성
+                GameObject index = Instantiate(piecePrefab, tempPosition, Quaternion.identity) as GameObject;
+                index.transform.parent = this.transform;
+                index.name = "[ " + row + " , " + column + " ] ";
+
+                int color = Random.Range(0, pieces.Length);
+                GameObject piece = Instantiate(pieces[color], tempPosition, Quaternion.identity);
+                piece.transform.parent = transform;
+                piece.name = "[ " + row + " , " + column + " ] ";
+
+                allDots[row, column] = piece;
+            }
+        }
+    }
+}
